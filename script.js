3102436afdad3f561d44b907d36d3846d58e5963
Fran1409 <fran_andries@hotmail.com>
2020-11-20 10:10:29 +0100

    Add function to compare choices

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+    
     //declare variables
     let yourChoice = "";
     let computerChoice = "";   
     
     //get players choice
-    function choice_click(clicked) { 
+   function choice_click(clicked) { 
         yourChoice = clicked; 
         console.log(yourChoice);
     };   
@@ -25,10 +26,33 @@
         else if (i == 3) {
             computerChoice = "scissors"
         }
-        
-        console.log (i);
-        console.log(computerChoice);
+
+        return computerChoice;
+    };
+
+    //compare choices 
+    function compareChoices() {
+        computerPick();
+        console.log(yourChoice + computerChoice);
+
+        if ( yourChoice === computerChoice) {
+            console.log("tie");
+        }
+        else if ( yourChoice === "rock" && computerChoice !== "paper") {
+            console.log("you win with rock");
+        }
+        else if ( yourChoice === "paper" && computerChoice !== "scissors") {
+            console.log("you win with paper");
+        }
+        else if ( yourChoice === "scissors" && computerChoice !== "rock") {
+            console.log("you win with scissors");
+        }
+        else {
+            console.log("you lose");
+        };
     };
 
     //first get computers choice when play is clicked
-    document.getElementById("play").addEventListener("click", computerPick);
+    document.getElementById("play").addEventListener("click", compareChoices);
+
+    
