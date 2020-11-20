d737f55bb0844ecf3b47aa15ba02007ed71f201c
Fran1409 <fran_andries@hotmail.com>
2020-11-20 09:47:28 +0100

    Add functions to get computers choice

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
     //declare variables
-    let yourChoice = ""
-    let computerChoice = ""   
+    let yourChoice = "";
+    let computerChoice = "";   
     
+    //get players choice
     function choice_click(clicked) { 
         yourChoice = clicked; 
         console.log(yourChoice);
-    }   
+    };   
 
+    //get computers choice
+    function getRndNumber(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+      };
+
+    function computerPick () {
+        let i = getRndNumber(1, 4);
+
+        if ( i == 1) {
+            computerChoice = "rock"
+        }
+        else if (i == 2) {
+            computerChoice = "paper"
+        }
+        else if (i == 3) {
+            computerChoice = "scissors"
+        }
+        
+        console.log (i);
+        console.log(computerChoice);
+    };
+
+    //first get computers choice when play is clicked
+    document.getElementById("play").addEventListener("click", computerPick);
